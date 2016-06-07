960f3be840bd8d97840056c8b4d83fc005e7d8f0
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2016-06-07 17:25:04 +0100

    Speed up by reading list of percentiles

--- lf.py
+++ lf.py
@@ -38,20 +38,15 @@ elif case == 3:
 
 lfg = lf(quasar_files=qlumfiles, selection_maps=selnfiles)
 
-# g = (0.2, -6.7, 1.1, -29, 0.1, -2.4, -0.6, -3.5)
-# g = (-0.5, -3.5, -0.5, -23.5, -0.0, -3.0, -0.0, -2.0)
-
 g = (0.2, -6.7, 1.1, -29, -2.5, -2.0)
-# g = (-0.5, -3.5, -0.5, -23.5, -1.0,  -1.0) 
 
 b = lfg.bestfit(g, method=method)
-print b
+print b 
 
+# Uncomment if you want automatic priors 
 # lfg.create_param_range()
-import numpy as np 
-# lfg.prior_min_values = (-3.0, -10.0, -3.0, -40.0, -1.0, -5.0, -1.0, -5.0)
-# lfg.prior_max_values = ( 3.0,  -2.0,  3.0, -20.0,  1.0, 2.0,  1.0, 2.0)
 
+import numpy as np 
 lfg.prior_min_values = (-3.0, -10.0, -3.0, -40.0, -6.0, -3.0)
 lfg.prior_max_values = ( 3.0,  -2.0,  3.0, -20.0,  1.0,  2.0)
 
@@ -67,9 +62,14 @@ labels = [r'$a_0 [\phi_*]$', r'$a_1 [\phi_*]$',
 lfg.corner_plot(labels=labels)
 lfg.chains(labels=labels)
 
-# pg(lfg) 
+# Uncomment if you want full individual calculation 
+# import bins
 
-import bins
+sp(lfg, individuals='percs.npz')
 
-sp(lfg, individuals=bins.lfs)
-pg(lfg, individuals=bins.lfs) 
+# pg(lfg, individuals=bins.lfs) 
+
+from waic_composite import waic
+print 'WIAC=', waic(lfg)
+
+
