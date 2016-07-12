9cf00d7a8f8551198642ec75f695521bc698e41a
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2016-07-12 19:23:42 +0100

    Add plotting functions

--- rt.py
+++ rt.py
@@ -95,7 +95,7 @@ def emissivity(nu, z):
     
     return np.interp(l, wavelengths, emissivities_at_z(z)) # ergs/s/Mpc^3/Hz
 
-def j(nu0, z0, zmax, dz=0.1):
+def j(nu0, z0, zmax=6.0, dz=0.1):
 
     def integrand(z):
 
@@ -109,7 +109,202 @@ def j(nu0, z0, zmax, dz=0.1):
     
     return r # ergs/s/Mpc^2/Hz/sr 
 
-x = j(3.29e15, 2.0, 6.0, dz=0.1)
-print x 
+# x = j(3.29e15, 2.0, dz=0.1)
+# print x 
 
+def gamma_HI(z, numax=1.0e18, dnu=0.1):
 
+    nu0 = 3.288e15 # Hz
+    lognu_min = np.log(nu0) 
+    lognu_max = np.log(numax)
+
+    lognu = np.arange(lognu_min, lognu_max, dnu)
+
+    def integrand(lognu):
+
+        nu = np.exp(lognu) # Hz 
+        hplanck = 6.626069e-34 # Js
+        cmbympc = 3.24077928965e-25 
+        return nu * j(nu, z) * sigma_HI(nu) * cmbympc**2 / (hplanck * 1.0e7 * nu) # s^-1 sr^-1 Hz^-1 
+
+    g = np.array([integrand(n) for n in lognu])
+    r = np.trapz(g, x=lognu) # s^-1 sr^-1 
+    r *= 4.0*np.pi # s^-1 
+
+    return r # s^-1 
+
+# import time
+# t1 = time.time()
+# x = gamma_HI(2.0)
+# t2 = time.time()
+# print x
+# print t2-t1
+
+def gamma_HI_alt(z, numax=1.0e18):
+
+    nu0 = 3.288e15 # Hz
+    lognu_min = np.log(nu0) 
+    lognu_max = np.log(numax)
+
+    def integrand(lognu):
+
+        nu = np.exp(lognu) # Hz 
+        hplanck = 6.626069e-34 # Js
+        cmbympc = 3.24077928965e-25 
+        return nu * j(nu, z) * sigma_HI(nu) * cmbympc**2 / (hplanck * 1.0e7 * nu) # s^-1 sr^-1 
+
+    r = quad(integrand, lognu_min, lognu_max)
+    return r[0]*4.0*np.pi
+
+# import time
+# t1 = time.time()
+# x = gamma_HI_alt(2.0)
+# t2 = time.time()
+# print x
+# print t2-t1
+
+def plot_fHI():
+
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=3, width=1)
+
+    logNHI = np.arange(10.0, 22.0, 0.1)
+
+    fNHI = np.array([f(10.0**n, 2.0) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='k', label='$z=2$')
+
+    fNHI = np.array([f(10.0**n, 3.5) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='r', label='$z=3.5$')
+    
+    fNHI = np.array([f(10.0**n, 5.0) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='b', label='$z=5$')
+
+    plt.xlim(11, 21.5)
+    plt.ylim(-25, -7)
+    plt.xlabel(r'$\log_{10}\left[N_\mathrm{HI}/\mathrm{cm}^{-2}\right]$')
+    plt.ylabel(r'$\log_{10}\left[f(N_\mathrm{HI},z)/\mathrm{cm}^2\right]$')
+    
+    plt.legend(loc='lower left', fontsize=14, handlelength=3,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.4, borderpad=0.2, scatterpoints=1)
+
+    plt.savefig('fhi.pdf',bbox_inches='tight')
+
+    return
+
+def tau_integrand(log10N_HI, z):
+
+    N_HI = 10.0**log10N_HI # Different from above!  To match HM12.
+    nu0 = 3.288e15 # Hz
+    tau = sigma_HI(nu0)*N_HI 
+
+    i = N_HI * f(N_HI, z) * (1.0-np.exp(-tau))
+
+    return i 
+
+def plot_tau_integrand():
+
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=3, width=1)
+
+    logNHI = np.arange(10.0, 22.0, 0.1)
+
+    fNHI = np.array([tau_integrand(n, 2.0) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='k', label='$z=2$')
+
+    fNHI = np.array([tau_integrand(n, 3.5) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='r', label='$z=3.5$')
+    
+    fNHI = np.array([tau_integrand(n, 5.0) for n in logNHI])
+    logfNHI = np.log10(fNHI)
+    plt.plot(logNHI, logfNHI, lw=2, c='b', label='$z=5$')
+
+    plt.xlim(11, 21.5)
+    # plt.ylim(-25, -7)
+    plt.xlabel(r'$\log_{10}\left[N_\mathrm{HI}/\mathrm{cm}^{-2}\right]$')
+    plt.ylabel(r'$\log_{10}\left[N_\mathrm{HI}f(N_\mathrm{HI},z)(1-e^{-N_\mathrm{HI}\sigma_{912}})\right]$')
+    
+    plt.legend(loc='lower left', fontsize=14, handlelength=3,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.4, borderpad=0.2, scatterpoints=1)
+
+    plt.savefig('tint.pdf',bbox_inches='tight')
+
+    return
+    
+def plot_tau_vs_nu():
+
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=3, width=1)
+
+    nu0 = 3.288e15 # Hz
+    numax = 1.0e18 # Hz 
+    lognu_min = np.log10(nu0)
+    lognu_max = np.log10(numax)
+    lognu = np.arange(lognu_min, lognu_max, 0.1)
+    
+    tau = np.array([tau_eff(10.0**n, 2.0, 6.0) for n in lognu])
+    plt.plot(lognu, tau, lw=2, c='k', label='$z=2$')
+
+    tau = np.array([tau_eff(10.0**n, 3.5, 6.0) for n in lognu])
+    plt.plot(lognu, tau, lw=2, c='r', label='$z=3.5$')
+
+    tau = np.array([tau_eff(10.0**n, 5.0, 6.0) for n in lognu])
+    plt.plot(lognu, tau, lw=2, c='b', label='$z=5$')
+
+    # plt.xlim(11, 21.5)
+    # plt.ylim(-25, -7)
+    plt.xlabel(r'$\log_{10}\left[\nu/\mathrm{Hz}\right]$')
+    plt.ylabel(r'$\tau(\nu, z, 6.0)$')
+
+    plt.yscale('log')
+    
+    plt.legend(loc='upper right', fontsize=14, handlelength=3,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.4, borderpad=0.2, scatterpoints=1)
+
+    plt.savefig('taunu.pdf',bbox_inches='tight')
+
+def plot_tau_vs_z():
+
+    fig = plt.figure(figsize=(7, 7), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=3, width=1)
+
+    zs = np.arange(2.0, 6.0, 0.1)
+    nu0 = 3.288e15 # Hz
+
+    tau = np.array([tau_eff(nu0, r, 6.0) for r in zs])
+    plt.plot(zs, tau, lw=2, c='k', label=r'$\nu_{912}$')
+
+    tau = np.array([tau_eff(nu0*10.0, r, 6.0) for r in zs])
+    plt.plot(zs, tau, lw=2, c='r', label=r'$10\nu_{912}$')
+
+    tau = np.array([tau_eff(nu0*100.0, r, 6.0) for r in zs])
+    plt.plot(zs, tau, lw=2, c='b', label=r'$100\nu_{912}$')
+    
+    plt.xlabel(r'$z$')
+    plt.ylabel(r'$\tau(\nu, z, 6.0)$')
+
+    plt.yscale('log')
+    
+    plt.legend(loc='upper right', fontsize=14, handlelength=3,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.4, borderpad=0.2, scatterpoints=1)
+
+    plt.savefig('tauz.pdf',bbox_inches='tight')
+    
+
+plot_tau_vs_z() 
