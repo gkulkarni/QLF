b22ab8881457e6f5cc2351bd81a80dd7519ebcd8
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2016-07-20 02:33:16 +0100

    Plot photoionisation rate calculated using RT

--- gammapi.py
+++ gammapi.py
@@ -4,8 +4,9 @@ mpl.use('Agg')
 mpl.rcParams['text.usetex'] = True 
 mpl.rcParams['font.family'] = 'serif'
 mpl.rcParams['font.serif'] = 'cm'
-mpl.rcParams['font.size'] = '14'
+mpl.rcParams['font.size'] = '22'
 import matplotlib.pyplot as plt
+import rtg 
 
 def luminosity(M):
     return 10.0**((51.60-M)/2.5) # ergs s^-1 Hz^-1 
@@ -60,29 +61,89 @@ def Gamma_HI_singleslope(loglf, theta, z, fit='composite'):
     alpha_EUV = -1.7
     return 4.6e-13 * (em/1.0e24) * ((1.0+z)/5.0)**(-2.4) / (1.5-alpha_EUV) # s^-1
 
-def plot_gamma(composite, individuals=None, zlims=(2.0,6.5), dirname=''):
+def get_gamma_error(individuals):
 
-    mpl.rcParams['font.size'] = '14'
+    g_up = []
+    g_low = []
+
+    for x in individuals:
+
+        z = x.z.mean()
+
+        lowlim = np.percentile(x.samples, 15.87, axis=0)
+        rate = rtg.gamma_HI(z, x.log10phi, lowlim, individual=True)
+        rate = np.log10(rate)+12.0
+        g_low.append(rate)
+
+        uplim = np.percentile(x.samples, 84.13, axis=0)
+        rate = rtg.gamma_HI(z, x.log10phi, uplim, individual=True)
+        rate = np.log10(rate)+12.0
+        g_up.append(rate)
+        
+    g_up = np.array(g_up)
+    g_low = np.array(g_low)
+
+    return g_up, g_low 
     
-    zmin, zmax = zlims 
-    z = np.linspace(zmin, zmax, num=50) 
 
-    fig = plt.figure(figsize=(6, 6), dpi=100)
+def plot_gamma(composite, individuals=None, zlims=(2.0,6.5), dirname='', fast=True, rt=False, lsa=False):
+
+    """
+    Calculates and plots HI photoionization rate. 
+
+    rt   = True: uses the integral solution to the cosmological RT equation 
+    lsa  = True: uses local source approximation 
+    fast = True: reads stored values instead of calculating again 
+
+    """
+
+    zmin, zmax = zlims 
+    z = np.linspace(zmin, zmax, num=10) 
+
+    fig = plt.figure(figsize=(7, 7), dpi=100)
     ax = fig.add_subplot(1, 1, 1)
 
-    ax.tick_params('both', which='major', length=4, width=1)
-    ax.tick_params('both', which='minor', length=2, width=1)
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=5, width=1)
 
-    rindices = np.random.randint(len(composite.samples), size=900)
-    for theta in composite.samples[rindices]:
-        g = np.array([Gamma_HI(composite.log10phi, theta, rs) for rs in z])
+    if lsa:
+        rindices = np.random.randint(len(composite.samples), size=900)
+        for theta in composite.samples[rindices]:
+            g = np.array([Gamma_HI(composite.log10phi, theta, rs) for rs in z])
+            g = np.log10(g)+12.0
+            ax.plot(z, g, color='#67a9cf', alpha=0.1, zorder=1)
+
+        bf = composite.samples.mean(axis=0)
+        g = np.array([Gamma_HI(composite.log10phi, bf, rs) for rs in z])
         g = np.log10(g)+12.0
-        ax.plot(z, g, color='#67a9cf', alpha=0.1, zorder=1)
+        ax.plot(z, g, color='k', zorder=2)
 
-    bf = composite.samples.mean(axis=0)
-    g = np.array([Gamma_HI(composite.log10phi, theta, rs) for rs in z])
-    g = np.log10(g)+12.0
-    ax.plot(z, g, color='k', zorder=2)
+    if rt:
+        if fast:
+            data = np.load('gammapi.npz')
+            z = data['z']
+            g = data['g']
+            g_lowlim = data['g_lowlim']
+            g_uplim = data['g_uplim']
+            ax.fill_between(z, g_lowlim, g_uplim, color='#67a9cf', alpha=0.3, edgecolor='None', zorder=1) 
+            ax.plot(z, g, color='k', zorder=2)
+        else:
+            z = np.linspace(zmin, zmax, num=10) 
+
+            lowlim = np.percentile(composite.samples, 15.87, axis=0)
+            g_lowlim = np.array([rtg.gamma_HI(rs, composite.log10phi, lowlim) for rs in z])
+            g_lowlim = np.log10(g_lowlim)+12.0
+            uplim = np.percentile(composite.samples, 84.13, axis=0)
+            g_uplim = np.array([rtg.gamma_HI(rs, composite.log10phi, uplim) for rs in z])
+            g_uplim = np.log10(g_uplim)+12.0
+            ax.fill_between(z, g_lowlim, g_uplim, color='#67a9cf', alpha=0.3, edgecolor='None', zorder=1) 
+
+            bf = composite.samples.mean(axis=0)
+            g = np.array([rtg.gamma_HI(rs, composite.log10phi, bf) for rs in z])
+            g = np.log10(g)+12.0
+            ax.plot(z, g, color='k', zorder=2, dashes=[7,2], label='radiative transfer')
+
+            np.savez('gammapi', z=z, g=g, g_lowlim=g_lowlim, g_uplim=g_uplim)
 
     ax.set_ylabel(r'$\log_{10}(\Gamma_\mathrm{HI}/10^{-12} \mathrm{s}^{-1})$')
     ax.set_xlabel('$z$')
@@ -92,43 +153,75 @@ def plot_gamma(composite, individuals=None, zlims=(2.0,6.5), dirname=''):
     
     zm, gm, gm_up, gm_low = np.loadtxt('Data/BeckerBolton.dat',unpack=True) 
 
-    ax.errorbar(zm, gm, ecolor='#d7191c', capsize=0,
+    ax.scatter(zm, gm, c='#d7191c', edgecolor='None', label='Becker and Bolton 2013', s=32)
+    ax.errorbar(zm, gm, ecolor='#d7191c', capsize=5,
                 yerr=np.vstack((gm_up, abs(gm_low))),
-                fmt='o', zorder=3, mfc='#d7191c', mec='#d7191c',
-                mew=1, ms=5, label='Becker and Bolton 2013')
+                fmt='None', zorder=3, mfc='#d7191c', mec='#d7191c',
+                mew=1, ms=5)
 
     zm, gm, gm_sigma = np.loadtxt('Data/calverley.dat',unpack=True) 
-    gm += 12.0 
-    ax.errorbar(zm, gm, ecolor='#fdae61', capsize=0,
-                yerr=gm_sigma, fmt='o', zorder=4, mfc='#fdae61',
-                mec='#fdae61', mew=1, ms=5, label='Calverley et al.~2011')
+    gm += 12.0
+    ax.scatter(zm, gm, c='#99cc66', edgecolor='None', label='Calverley et al.~2011', s=32) 
+    ax.errorbar(zm, gm, ecolor='#99CC66', capsize=5,
+                yerr=gm_sigma, fmt='None', zorder=4, mfc='#99CC66',
+                mec='#99CC66', mew=1, ms=5)
 
-    if individuals is not None:
-        c = np.array([x.gammapi[2]+12.0 for x in individuals])
-        u = np.array([x.gammapi[0]+12.0 for x in individuals])
-        l = np.array([x.gammapi[1]+12.0 for x in individuals])
-        uyerr = u-c
-        lyerr = c-l 
+    if lsa:
+        if individuals is not None:
+            c = np.array([x.gammapi[2]+12.0 for x in individuals])
+            u = np.array([x.gammapi[0]+12.0 for x in individuals])
+            l = np.array([x.gammapi[1]+12.0 for x in individuals])
+            uyerr = u-c
+            lyerr = c-l 
 
-        zs = np.array([x.z.mean() for x in individuals])
-        uz = np.array([x.z.max() for x in individuals])
-        lz = np.array([x.z.min() for x in individuals])
-        uzerr = uz-zs
-        lzerr = zs-lz 
+            zs = np.array([x.z.mean() for x in individuals])
+            uz = np.array([x.z.max() for x in individuals])
+            lz = np.array([x.z.min() for x in individuals])
+            uzerr = uz-zs
+            lzerr = zs-lz 
+
+            ax.errorbar(zs, c, ecolor='#404040', capsize=0,
+                        yerr=np.vstack((uyerr,lyerr)),
+                        xerr=np.vstack((lzerr,uzerr)), fmt='o',
+                        mfc='#ffffff', mec='#404040', zorder=3, mew=1,
+                        ms=5, label='Individual Fits')
+
+    if rt:
+        if individuals is not None:
+            zs = np.array([x.z.mean() for x in individuals])
+            uz = np.array([x.z.max() for x in individuals])
+            lz = np.array([x.z.min() for x in individuals])
+            uzerr = uz-zs
+            lzerr = zs-lz 
+            print zs
+
+            g = []
+            for x in individuals:
+                z = x.z.mean()
+                bf = x.samples.mean(axis=0)
+                rate = rtg.gamma_HI(z, x.log10phi, bf, individual=True)
+                rate = np.log10(rate)+12.0
+                g.append(rate)
+
+            g = np.array(g)
+            ax.scatter(zs, g, s=32, c='#ffffff', edgecolor='#404040', zorder=3, label='Individual fits')
+
+            g_up, g_low = get_gamma_error(individuals)
+            uyerr = g_up-g
+            lyerr = g-g_low
+            ax.errorbar(zs, g, ecolor='#404040', capsize=0,
+                        yerr=np.vstack((uyerr,lyerr)),
+                        xerr=np.vstack((lzerr,uzerr)),
+                        fmt='None',zorder=2)
+
+            np.savez('gammapi_individuals', zs=zs, g=g, uzerr=uzerr, lzerr=lzerr, uyerr=uyerr, lyerr=lyerr)
         
-        ax.errorbar(zs, c, ecolor='#404040', capsize=0,
-                    yerr=np.vstack((uyerr,lyerr)),
-                    xerr=np.vstack((lzerr,uzerr)), fmt='o',
-                    mfc='#ffffff', mec='#404040', zorder=3, mew=1,
-                    ms=5, label='Individual Fits')
-
-    plt.legend(loc='lower left',fontsize=12,handlelength=3,frameon=False,framealpha=0.0,
-            labelspacing=.1,handletextpad=0.4,borderpad=0.2,numpoints=1)
+    plt.legend(loc='lower left', fontsize=14, handlelength=1,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.1, borderpad=0.01, scatterpoints=1)
     
     plt.savefig('gammapi.pdf',bbox_inches='tight')
-
-    mpl.rcParams['font.size'] = '22'
+    plt.close('all')
 
     return
-
 
