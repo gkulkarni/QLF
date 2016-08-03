fe6589f4294ad32f81ca9c56731368740027b523
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2016-08-03 12:29:37 +0100

    Add Kashikawa quasars

--- bins.py
+++ bins.py
@@ -5,7 +5,7 @@ from individual import lf
 from summary import get_percentiles
 
 # Defaults 
-case = 4
+case = 5
 method = 'Nelder-Mead'
 
 if case == 1:
@@ -23,11 +23,12 @@ elif case == 2:
                  ('Data/sdss_selfunc2.dat',6248.0,13)]
 
 elif case == 3: 
-
+    
     qlumfiles = ['Data/bossdr9color.dat',
                  'Data/dr7z3p7.dat',
                  'Data/glikman11qso.dat']
-    
+
+    # Note: Glikman entries wrong; see case = 5 instead.
     selnfiles = [('Data/ross13_selfunc2.dat',2236.0,1),
                  ('Data/sdss_selfunc2.dat',6248.0,13),
                  ('Data/glikman11_selfunc_dls.dat',1.71,15),
@@ -36,20 +37,49 @@ elif case == 3:
 elif case == 4: 
 
     qlumfiles = ['Data/bossdr9color.dat',
-                 'Data/new_data/dr7z3p7.dat',
+                 'Data/dr7z3p7.dat',
                  'Data/glikman11qso.dat',
-                 'Data/new_data/mcgreer13_s82sample.dat',
-                 'Data/new_data/mcgreer13_dr7sample.dat',
-                 'Data/new_data/mcgreer13_s82extend.dat']
-    
+                 'Data/mcgreer13_s82sample.dat',
+                 'Data/mcgreer13_dr7sample.dat',
+                 'Data/mcgreer13_s82extend.dat']
+
+    # Note: Glikman entries wrong; see case = 5 instead.    
     selnfiles = [('Data/ross13_selfunc2.dat',2236.0,1),
-                 ('Data/new_data/sdss_selfunchigh.dat',6248.0,13),
+                 ('Data/sdss_selfunchigh.dat',6248.0,13),
                  ('Data/glikman11_selfunc_dls.dat',1.71,15),
                  ('Data/glikman11_selfunc_ndwfs.dat',2.05,6),
-                 ('Data/new_data/mcgreer13_s82selfunc.dat',235.0,8),
-                 ('Data/new_data/mcgreer13_dr7selfunc.dat',6248.0,16)]
+                 ('Data/mcgreer13_s82selfunc.dat',235.0,8),
+                 ('Data/mcgreer13_dr7selfunc.dat',6248.0,16)]
+
+elif case == 5: 
+
+    qlumfiles = ['Data/bossdr9color.dat',
+                 'Data/dr7z3p7.dat',
+                 'Data/glikman11qso.dat',
+                 'Data/mcgreer13_s82sample.dat',
+                 'Data/mcgreer13_dr7sample.dat',
+                 'Data/mcgreer13_s82extend.dat',
+                 'Data/fan06_sample.dat',
+                 'Data/jiang08_sample.dat',
+                 'Data/jiang09_sample.dat',
+                 'Data/willott10_cfhqsdeepsample.dat',
+                 'Data/willott10_cfhqsvwsample.dat',
+                 'Data/kashikawa15_sample.dat']
     
-z = np.array([2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.5, 4.0, 4.5, 5.0])
+    selnfiles = [('Data/ross13_selfunc2.dat',2236.0,1),
+                 ('Data/sdss_selfunchigh.dat',6248.0,13),
+                 ('Data/glikman11_selfunc_ndwfs.dat',1.71,15),
+                 ('Data/glikman11_selfunc_dls.dat',2.05,6),
+                 ('Data/mcgreer13_s82selfunc.dat',235.0,8),
+                 ('Data/mcgreer13_dr7selfunc.dat',6248.0,16),
+                 ('Data/fan06_sel.dat',6600.0,17),
+                 ('Data/jiang08_sel.dat',260.0,9),
+                 ('Data/jiang09_sel.dat',195.0,18),
+                 ('Data/willott10_cfhqsvwsel.dat',494.0,19),
+                 ('Data/willott10_cfhqsdeepsel.dat',4.47,10),
+                 ('Data/kashikawa15_sel.dat',6.50,11)]
+    
+z = np.array([2.2, 2.3, 2.4, 2.5, 2.6, 2.7, 2.8, 2.9, 3.0, 3.5, 4.0, 4.5, 5.5, 7.0])
 
 lfs = [] 
 
@@ -71,8 +101,8 @@ for i, rs in enumerate(z[:-1]):
 
     lfi.run_mcmc()
     lfi.get_percentiles()
-    lfi.draw(lfi.z.mean(), dirname='set5/', plotlit=True)
-    lfi.get_gammapi_percentiles(lfi.z.mean()) 
+    lfi.draw(lfi.z.mean(), dirname='set7/', plotlit=True)
+    lfi.get_gammapi_percentiles(lfi.z.mean(),rt=False) 
     
     lfs.append(lfi)
 
@@ -83,6 +113,6 @@ if write_percentiles:
     m_star   = get_percentiles(lfs, param=2, individuals_isfile=False)
     alpha    = get_percentiles(lfs, param=3, individuals_isfile=False)
     beta     = get_percentiles(lfs, param=4, individuals_isfile=False)
-    np.savez('percs_newdata.npz', zs=zs, phi_star=phi_star, m_star=m_star,
+    np.savez('percs_full.npz', zs=zs, phi_star=phi_star, m_star=m_star,
              alpha=alpha, beta=beta)
     
