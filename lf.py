63354ea664c0292b5726db174f947c1d250268a7
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2016-10-18 14:59:30 +0100

    Add new quasars

--- lf.py
+++ lf.py
@@ -3,6 +3,7 @@ import sys
 import time
 from summary import summary_plot as sp
 from gammapi import plot_gamma as pg 
+import numpy as np
 
 # Defaults 
 case = 3
@@ -79,23 +80,83 @@ elif case == 5:
                  ('Data/willott10_cfhqsvwsel.dat',494.0),
                  ('Data/willott10_cfhqsdeepsel.dat',4.47),
                  ('Data/kashikawa15_sel.dat',6.50)]
+
+elif case == 6: 
+
+    qlumfiles = ['Data/bossdr9color.dat',
+                 'Data/dr7z3p7.dat',
+                 'Data/glikman11qso.dat',
+                 'Data/mcgreer13_s82sample.dat',
+                 'Data/mcgreer13_dr7sample.dat',
+                 'Data/mcgreer13_s82extend.dat',
+                 'Data/fan06_sample.dat',
+                 'Data/jiang08_sample.dat',
+                 'Data/jiang09_sample.dat',
+                 'Data/willott10_cfhqsdeepsample.dat',
+                 'Data/willott10_cfhqsvwsample.dat',
+                 'Data/kashikawa15_sample.dat',
+                 'Data/dr7z2p2.dat']
+    
+    selnfiles = [('Data/ross13_selfunc2.dat',2236.0),
+                 ('Data/sdss_selfunchigh.dat',6248.0),
+                 ('Data/glikman11_selfunc_dls.dat',1.71),
+                 ('Data/glikman11_selfunc_ndwfs.dat',2.05),
+                 ('Data/mcgreer13_s82selfunc.dat',235.0),
+                 ('Data/mcgreer13_dr7selfunc.dat',6248.0),
+                 ('Data/fan06_sel.dat',6600.0),
+                 ('Data/jiang08_sel.dat',260.0),
+                 ('Data/jiang09_sel.dat',195.0),
+                 ('Data/willott10_cfhqsvwsel.dat',494.0),
+                 ('Data/willott10_cfhqsdeepsel.dat',4.47),
+                 ('Data/kashikawa15_sel.dat',6.50),
+                 ('Data/sdss_selfunclow.dat',6248.0)]
+
+elif case == 7:
+
+    # All high-z (z >= 2.2) quasars; includes Giallongo 
+
+    qlumfiles = ['Data/bossdr9color.dat',
+                 'Data/dr7z3p7.dat',
+                 'Data/glikman11qso.dat',
+                 'Data/mcgreer13_s82sample.dat',
+                 'Data/mcgreer13_dr7sample.dat',
+                 'Data/mcgreer13_s82extend.dat',
+                 'Data/fan06_sample.dat',
+                 'Data/jiang08_sample.dat',
+                 'Data/jiang09_sample.dat',
+                 'Data/willott10_cfhqsdeepsample.dat',
+                 'Data/willott10_cfhqsvwsample.dat',
+                 'Data/kashikawa15_sample.dat',
+                 'Data/giallongo15_sample.dat']
+    
+    selnfiles = [('Data/ross13_selfunc2.dat',2236.0),
+                 ('Data/sdss_selfunchigh.dat',6248.0),
+                 ('Data/glikman11_selfunc_dls.dat',1.71),
+                 ('Data/glikman11_selfunc_ndwfs.dat',2.05),
+                 ('Data/mcgreer13_s82selfunc.dat',235.0),
+                 ('Data/mcgreer13_dr7selfunc.dat',6248.0),
+                 ('Data/fan06_sel.dat',6600.0),
+                 ('Data/jiang08_sel.dat',260.0),
+                 ('Data/jiang09_sel.dat',195.0),
+                 ('Data/willott10_cfhqsvwsel.dat',494.0),
+                 ('Data/willott10_cfhqsdeepsel.dat',4.47),
+                 ('Data/kashikawa15_sel.dat',6.50),
+                 ('Data/giallongo15_sel.dat',0.047)]
+    
     
 lfg = lf(quasar_files=qlumfiles, selection_maps=selnfiles, pnum=[3,3,2,3])
+
 # g = [-6.7, -1.8, 0.2, -26.0, -1.8, 0.2, -5.0, -1.8, -0.2, -1.6, -1.8, 0.6]
 g = [-6.7, -1.8, 0.2, -26.0, -1.8, 0.2, -5.0, -1.8, -1.6, -1.8, 0.6]
 
 b = lfg.bestfit(g, method=method)
 print b
 
-sys.exit()
-
 # Uncomment if you want automatic priors 
 # lfg.create_param_range()
 
-import numpy as np 
-
-lfg.prior_min_values = np.array([-8.0, -3.0, -2.0, -30.0, -3.0, -1.0, -5.0, -3.0, -1.0, -3.0, -1.0, -1.0])
-lfg.prior_max_values = np.array([-5.0, -0.1,  0.4, -20.0,  1.0,  2.0, -1.0,  3.0,  3.0, -0.1,  1.0,  1.5])
+# lfg.prior_min_values = np.array([-8.0, -3.0, -2.0, -30.0, -3.0, -1.0, -5.0, -3.0, -1.0, -3.0, -1.0, -1.0])
+# lfg.prior_max_values = np.array([-5.0, -0.1,  0.4, -20.0,  1.0,  2.0, -1.0,  3.0,  3.0, -0.1,  1.0,  1.5])
 
 lfg.prior_min_values = np.array([-8.0, -3.0, -2.0, -30.0, -3.0, -1.0, -5.0, -3.0, -3.0, -1.0, -1.0])
 lfg.prior_max_values = np.array([-5.0, -0.1,  0.4, -20.0,  1.0,  2.0, -1.0,  0.0, -0.1,  1.0,  1.5])
@@ -108,6 +169,9 @@ labels = [r'$a_0 [\phi_*]$', r'$a_1 [\phi_*]$', r'$a_2 [\phi_*]$', r'$a_0 [M_*]$
 
 lfg.corner_plot(labels=labels)
 lfg.chains(labels=labels)
+
+sys.exit() 
+
 
 # Uncomment if you want full individual calculation 
 import bins
