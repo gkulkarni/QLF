99eed27f939a157b2b0bb9b2cc9953c32006a72b
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-01-12 10:11:35 +0000

    Run LF fitting code

--- lfi.py
+++ lfi.py
@@ -10,10 +10,22 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
              ('Data_new/croom09ngp_selfunc.dat', 0.3, 0.05, 127.7, 2)]
 
-zl = (0.1, 0.2)
+method = 'Nelder-Mead'
+zl = (1.0, 1.5)
 lfi = lf(quasar_files=qlumfiles, selection_maps=selnfiles, zlims=zl)
 
+g = (np.log10(1.e-6), -25.0, -3.0, -1.5)
+b = lfi.bestfit(g, method=method)
+print b
 
+lfi.prior_min_values = np.array([-8.0, -28.0, -7.0, -3.0])
+lfi.prior_max_values = np.array([-5.0, -22.0, -2.0, -1.0])
+assert(np.all(lfi.prior_min_values < lfi.prior_max_values))
 
+lfi.run_mcmc()
 
+lfi.get_percentiles()
+lfi.corner_plot()
+lfi.chains()
 
+lfi.draw(lfi.z.mean())
