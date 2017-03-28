831ffd863cfa3d9566d51f74d29f31a1c838c1d5
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-03-28 15:36:18 +0100

    Remove z > 5 points

--- bins.py
+++ bins.py
@@ -3,6 +3,43 @@ import numpy as np
 from individual import lf
 import mosaic
 
+# qlumfiles = ['Data_new/dr7z2p2_sample.dat',
+#              'Data_new/croom09sgp_sample.dat',
+#              'Data_new/croom09ngp_sample.dat',
+#              'Data_new/bossdr9color.dat',
+#              'Data_new/dr7z3p7_sample.dat',
+#              'Data_new/glikman11debug.dat',
+#              'Data_new/yang16_sample.dat',
+#              'Data_new/mcgreer13_dr7sample.dat',
+#              'Data_new/mcgreer13_s82sample.dat',
+#              'Data_new/mcgreer13_dr7extend.dat',
+#              'Data_new/mcgreer13_s82extend.dat',
+#              'Data_new/jiang16main_sample.dat',
+#              'Data_new/jiang16overlap_sample.dat',
+#              'Data_new/jiang16s82_sample.dat',
+#              'Data_new/willott10_cfhqsdeepsample.dat',
+#              'Data_new/willott10_cfhqsvwsample.dat',
+#              'Data_new/kashikawa15_sample.dat',
+#              'Data_new/giallongo15_sample.dat']
+
+# selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13, r'SDSS DR7 Richards et al.\ 2006'),
+#              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15, r'2SLAQ Croom et al.\ 2009'),
+#              ('Data_new/croom09ngp_selfunc.dat', 0.3, 0.05, 127.7, 15, r'2SLAQ Croom et al.\ 2009'),
+#              ('Data_new/ross13_selfunc2.dat', 0.1, 0.05, 2236.0, 1, r'BOSS DR9 Ross et al.\ 2013'),
+#              ('Data_new/dr7z3p7_selfunc.dat', 0.1, 0.05, 6248.0, 13, r'SDSS DR7 Richards et al.\ 2006'),
+#              ('Data_new/glikman11_selfunc_ndwfs.dat', 0.05, 0.02, 1.71, 6, r'NDWFS+DLS Glikman et al.\ 2011'),
+#              ('Data_new/glikman11_selfunc_dls.dat', 0.05, 0.02, 2.05, 6, r'NDWFS+DLS Glikman et al.\ 2011'),
+#              ('Data_new/yang16_sel.dat', 0.1, 0.05, 14555.0, 17, r'SDSS+WISE Yang et al.\ 2016'),
+#              ('Data_new/mcgreer13_dr7selfunc.dat', 0.1, 0.05, 6248.0, 8, r'SDSS+S82 McGreer et al.\ 2013'),
+#              ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8, r'SDSS+S82 McGreer et al.\ 2013'),
+#              ('Data_new/jiang16main_selfunc.dat', 0.1, 0.05, 11240.0, 18, r'SDSS Jiang et al.\ 2016'),
+#              ('Data_new/jiang16overlap_selfunc.dat', 0.1, 0.05, 4223.0, 18, r'SDSS Jiang et al.\ 2016'),
+#              ('Data_new/jiang16s82_selfunc.dat', 0.1, 0.05, 277.0, 18, r'SDSS Jiang et al.\ 2016'),
+#              ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10, r'CFHQS Willott et al.\ 2010'),
+#              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10, r'CFHQS Willott et al.\ 2010'),
+#              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11, r'Subaru Kashikawa et al.\ 2015'),
+#              ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7, 'Giallongo et al.\ 2015')]
+
 qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/croom09sgp_sample.dat',
              'Data_new/croom09ngp_sample.dat',
@@ -13,14 +50,7 @@ qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/mcgreer13_dr7sample.dat',
              'Data_new/mcgreer13_s82sample.dat',
              'Data_new/mcgreer13_dr7extend.dat',
-             'Data_new/mcgreer13_s82extend.dat',
-             'Data_new/jiang16main_sample.dat',
-             'Data_new/jiang16overlap_sample.dat',
-             'Data_new/jiang16s82_sample.dat',
-             'Data_new/willott10_cfhqsdeepsample.dat',
-             'Data_new/willott10_cfhqsvwsample.dat',
-             'Data_new/kashikawa15_sample.dat',
-             'Data_new/giallongo15_sample.dat']
+             'Data_new/mcgreer13_s82extend.dat']
 
 selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13, r'SDSS DR7 Richards et al.\ 2006'),
              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15, r'2SLAQ Croom et al.\ 2009'),
@@ -31,21 +61,23 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13, r'SDSS DR7 
              ('Data_new/glikman11_selfunc_dls.dat', 0.05, 0.02, 2.05, 6, r'NDWFS+DLS Glikman et al.\ 2011'),
              ('Data_new/yang16_sel.dat', 0.1, 0.05, 14555.0, 17, r'SDSS+WISE Yang et al.\ 2016'),
              ('Data_new/mcgreer13_dr7selfunc.dat', 0.1, 0.05, 6248.0, 8, r'SDSS+S82 McGreer et al.\ 2013'),
-             ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8, r'SDSS+S82 McGreer et al.\ 2013'),
-             ('Data_new/jiang16main_selfunc.dat', 0.1, 0.05, 11240.0, 18, r'SDSS Jiang et al.\ 2016'),
-             ('Data_new/jiang16overlap_selfunc.dat', 0.1, 0.05, 4223.0, 18, r'SDSS Jiang et al.\ 2016'),
-             ('Data_new/jiang16s82_selfunc.dat', 0.1, 0.05, 277.0, 18, r'SDSS Jiang et al.\ 2016'),
-             ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10, r'CFHQS Willott et al.\ 2010'),
-             ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10, r'CFHQS Willott et al.\ 2010'),
-             ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11, r'Subaru Kashikawa et al.\ 2015'),
-             ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7, 'Giallongo et al.\ 2015')]
+             ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8, r'SDSS+S82 McGreer et al.\ 2013')]
+
+             # ('Data_new/jiang16main_selfunc.dat', 0.1, 0.05, 11240.0, 18, r'SDSS Jiang et al.\ 2016'),
+             # ('Data_new/jiang16overlap_selfunc.dat', 0.1, 0.05, 4223.0, 18, r'SDSS Jiang et al.\ 2016'),
+             # ('Data_new/jiang16s82_selfunc.dat', 0.1, 0.05, 277.0, 18, r'SDSS Jiang et al.\ 2016'),
+             # ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10, r'CFHQS Willott et al.\ 2010'),
+             # ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10, r'CFHQS Willott et al.\ 2010'),
+             # ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11, r'Subaru Kashikawa et al.\ 2015'),
+             # ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7, 'Giallongo et al.\ 2015')]
+
 
 method = 'Nelder-Mead'
 
 zls = [(0.6, 0.68), (0.68, 1.06), (1.06, 1.44), (1.44, 1.82), (1.82, 2.2),
        (2.2, 2.3), (2.3, 2.4), (2.4, 2.5), (2.5, 2.6), (2.6, 2.7), (2.7, 2.8),
        (2.8, 3.0), (3.0, 3.25), (3.25, 3.5), (3.7, 4.1), (4.1, 4.7),
-       (4.7, 5.5), (5.5, 6.5)]
+       (4.7, 5.5)] # , (5.5, 6.5)]
 
 lfs = [] 
 
@@ -54,6 +86,7 @@ for i, zl in enumerate(zls):
     print 'z =', zl
 
     lfi = lf(quasar_files=qlumfiles, selection_maps=selnfiles, zlims=zl)
+
     print '{:d} quasars in this bin.'.format(lfi.z.size)
 
     print 'sids: '+'  '.join(['{:2d}'.format(x.sid) for x in lfi.maps])
@@ -73,7 +106,7 @@ for i, zl in enumerate(zls):
     lfi.run_mcmc()
     lfi.get_percentiles()
 
-    write=True
+    write=False
     if write: 
         with open('phi_star.dat', 'a') as f:
             f.write(('{:.3f} '*6).format(lfi.z.mean(), zl[0], zl[1],
@@ -97,4 +130,4 @@ for i, zl in enumerate(zls):
             
     lfs.append(lfi)
 
-# mosaic.draw(lfs)
+    # mosaic.draw(lfs)
