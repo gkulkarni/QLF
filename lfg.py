01e28ffa2d165322fbd637aa3f4849c4e7810892
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-04-03 11:30:45 +0100

    Add z ~ 6 qsos to global fit

--- lfg.py
+++ lfg.py
@@ -3,43 +3,6 @@ import numpy as np
 from composite import lf
 from summary_fromFile import summary_plot as sp
 
-# qlumfiles = ['Data_new/dr7z2p2_sample.dat',
-#              'Data_new/croom09sgp_sample.dat',
-#              'Data_new/croom09ngp_sample.dat',
-#              'Data_new/bossdr9color.dat',
-#              'Data_new/dr7z3p7_sample.dat',
-#              'Data_new/glikman11debug.dat',
-#              'Data_new/yang16_sample.dat',
-#              'Data_new/mcgreer13_dr7sample.dat',
-#              'Data_new/mcgreer13_s82sample.dat',
-#              'Data_new/mcgreer13_dr7extend.dat',
-#              'Data_new/mcgreer13_s82extend.dat',
-#              'Data_new/jiang16main_sample.dat',
-#              'Data_new/jiang16overlap_sample.dat',
-#              'Data_new/jiang16s82_sample.dat',
-#              'Data_new/willott10_cfhqsdeepsample.dat',
-#              'Data_new/willott10_cfhqsvwsample.dat',
-#              'Data_new/kashikawa15_sample.dat',
-#              'Data_new/giallongo15_sample.dat']
-
-# selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
-#              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
-#              ('Data_new/croom09ngp_selfunc.dat', 0.3, 0.05, 127.7, 15),
-#              ('Data_new/ross13_selfunc2.dat', 0.1, 0.05, 2236.0, 1),
-#              ('Data_new/dr7z3p7_selfunc.dat', 0.1, 0.05, 6248.0, 13),
-#              ('Data_new/glikman11_selfunc_ndwfs.dat', 0.05, 0.02, 1.71, 6),
-#              ('Data_new/glikman11_selfunc_dls.dat', 0.05, 0.02, 2.05, 6),
-#              ('Data_new/yang16_sel.dat', 0.1, 0.05, 14555.0, 17),
-#              ('Data_new/mcgreer13_dr7selfunc.dat', 0.1, 0.05, 6248.0, 8),
-#              ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8),
-#              ('Data_new/jiang16main_selfunc.dat', 0.1, 0.05, 11240.0, 18),
-#              ('Data_new/jiang16overlap_selfunc.dat', 0.1, 0.05, 4223.0, 18),
-#              ('Data_new/jiang16s82_selfunc.dat', 0.1, 0.05, 277.0, 18),
-#              ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10),
-#              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
-#              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
-#              ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7)]
-
 qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/croom09sgp_sample.dat',
              'Data_new/croom09ngp_sample.dat',
@@ -50,7 +13,14 @@ qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/mcgreer13_dr7sample.dat',
              'Data_new/mcgreer13_s82sample.dat',
              'Data_new/mcgreer13_dr7extend.dat',
-             'Data_new/mcgreer13_s82extend.dat']
+             'Data_new/mcgreer13_s82extend.dat',
+             'Data_new/jiang16main_sample.dat',
+             'Data_new/jiang16overlap_sample.dat',
+             'Data_new/jiang16s82_sample.dat',
+             'Data_new/willott10_cfhqsdeepsample.dat',
+             'Data_new/willott10_cfhqsvwsample.dat',
+             'Data_new/kashikawa15_sample.dat',
+             'Data_new/giallongo15_sample.dat']
 
 selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
@@ -61,7 +31,37 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/glikman11_selfunc_dls.dat', 0.05, 0.02, 2.05, 6),
              ('Data_new/yang16_sel.dat', 0.1, 0.05, 14555.0, 17),
              ('Data_new/mcgreer13_dr7selfunc.dat', 0.1, 0.05, 6248.0, 8),
-             ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8)]
+             ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8),
+             ('Data_new/jiang16main_selfunc.dat', 0.1, 0.05, 11240.0, 18),
+             ('Data_new/jiang16overlap_selfunc.dat', 0.1, 0.05, 4223.0, 18),
+             ('Data_new/jiang16s82_selfunc.dat', 0.1, 0.05, 277.0, 18),
+             ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10),
+             ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
+             ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
+             ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7)]
+
+# qlumfiles = ['Data_new/dr7z2p2_sample.dat',
+#              'Data_new/croom09sgp_sample.dat',
+#              'Data_new/croom09ngp_sample.dat',
+#              'Data_new/bossdr9color.dat',
+#              'Data_new/dr7z3p7_sample.dat',
+#              'Data_new/glikman11debug.dat',
+#              'Data_new/yang16_sample.dat',
+#              'Data_new/mcgreer13_dr7sample.dat',
+#              'Data_new/mcgreer13_s82sample.dat',
+#              'Data_new/mcgreer13_dr7extend.dat',
+#              'Data_new/mcgreer13_s82extend.dat']
+
+# selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
+#              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
+#              ('Data_new/croom09ngp_selfunc.dat', 0.3, 0.05, 127.7, 15),
+#              ('Data_new/ross13_selfunc2.dat', 0.1, 0.05, 2236.0, 1),
+#              ('Data_new/dr7z3p7_selfunc.dat', 0.1, 0.05, 6248.0, 13),
+#              ('Data_new/glikman11_selfunc_ndwfs.dat', 0.05, 0.02, 1.71, 6),
+#              ('Data_new/glikman11_selfunc_dls.dat', 0.05, 0.02, 2.05, 6),
+#              ('Data_new/yang16_sel.dat', 0.1, 0.05, 14555.0, 17),
+#              ('Data_new/mcgreer13_dr7selfunc.dat', 0.1, 0.05, 6248.0, 8),
+#              ('Data_new/mcgreer13_s82selfunc.dat', 0.1, 0.05, 235.0, 8)]
 
 lfg = lf(quasar_files=qlumfiles, selection_maps=selnfiles, pnum=[3,2,2,3])
 
