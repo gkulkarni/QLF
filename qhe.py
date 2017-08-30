a8fdf8dba50972191d65cfab2f99b5c1e5d689dc
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-08-30 13:35:31 +0100

    He II and He III reionization
    This code is incomplete.  There is currently no coupling between H and
    He.  But it works.

--- qhe.py
+++ qhe.py
@@ -43,6 +43,7 @@ cmbympccb = 3.4036771916e-74 # (cm/Mpc)^3 conversion factor
 yrbys = 3.154e7 # yr/s conversion factor 
 
 nH = rho_critical * 1.0e10 * msolkg * omega_b * (1-Y_He) / mproton # Mpc^-3
+nHe = rho_critical * 1.0e10 * msolkg * omega_b * Y_He / (4*mproton) # Mpc^-3
 
 def qso_emissivity_MH15(z):
 
@@ -106,7 +107,8 @@ def clumping_factor(z):
 
     return 2.9 * ((1.0+z)/6.0)**-1.1
 
-def alpha_B_Draine2011(T):
+
+def alpha_B_HII_Draine2011(T):
     """
     Draine equation 14.6 
 
@@ -114,15 +116,81 @@ def alpha_B_Draine2011(T):
 
     return 2.54e-13*(T/1.0e4)**(-0.8163-0.0208*np.log(T/1.0e4)) # cm^3 s^-1
 
-def trec(z):
+
+def alpha_B_HII_HuiGnedin1997(T):
+    """Case B H II recombination coefficient. 
+    
+    Hui and Gnedin 1997 (MNRAS 292 27; Appendix A).
+
+    """
+
+    t_HI = 1.57807E5 # K; H ionization threshold 
+    reclambda = 2.0*t_HI/T
+    
+    return (2.753e-14 * (reclambda**1.5) /
+            (1.0 + (reclambda/2.74)**0.407)**2.242) # cm^3 s^-1 
+
+
+def alpha_B_HeII_HuiGnedin1997(T):
+    """Case B He II recombination coefficient.
+
+    Hui and Gnedin 1997 (MNRAS 292 27; Appendix A).
+
+    """
+
+    t_HeI = 2.85335E5 # K; He single ionization threshold 
+    reclambda = 2.0*t_HeI/T
+
+    return 1.26e-14 * reclambda**0.75 # cm^3 s^-1 
+    
+
+def alpha_B_HeIII_HuiGnedin1997(T):
+    """Case B He III recombination coefficient.
+
+    Hui and Gnedin 1997 (MNRAS 292 27; Appendix A).
+
+    """
+
+    t_HeII = 6.31515E5 # K; He single ionization threshold 
+    reclambda = 2.0*t_HeII/T
+
+    return (2.0 * 2.753e-14 *
+            (reclambda**1.5 /
+             (1.0 + (reclambda/2.740)**0.407)**2.242)) # cm^3 s^-1
+
+
+def trec_HII(z):
 
     chi = 0.083
     temperature = 2.0e4 # K 
-    alpha_r = alpha_B_Draine2011(temperature) # cm^3 s^-1
+    alpha_r = alpha_B_HII_Draine2011(temperature) # cm^3 s^-1
 
     r = (1+chi) * (1+z)**3 * alpha_r * nH * cmbympccb # s^-1
 
     return 1/r # s 
+
+
+def trec_HeII(z):
+
+    chi = 0.083
+    temperature = 2.0e4 # K 
+    alpha_r = alpha_B_HeII_HuiGnedin1997(temperature) # cm^3 s^-1
+
+    r = (1+chi) * (1+z)**3 * alpha_r * nHe * cmbympccb # s^-1
+
+    return 1/r # s 
+
+
+def trec_HeIII(z):
+
+    chi = 0.083
+    temperature = 2.0e4 # K 
+    alpha_r = alpha_B_HeIII_HuiGnedin1997(temperature) # cm^3 s^-1
+
+    r = (1+chi) * (1+z)**3 * alpha_r * nHe * cmbympccb # s^-1
+
+    return 1/r # s 
+
 
 def dqdt_HII(q, z):
 
@@ -131,10 +199,37 @@ def dqdt_HII(q, z):
     # Integrate qso emissivity from 1 to 4 Ry 
     n = (qso_emissivity_MH15(z)/(1.70*hplanck) -
          qso_emissivity_MH15(z)*0.25**1.7/(1.70*hplanck)) # s^-1 Mpc^-3 
-
-    d = n/nH - q/trec(z) # s^-1
+    
+    d = n/nH - q/trec_HII(z) # s^-1
 
     return d * yrbys # yr
+
+
+def dqdt_HeII(q, z):
+
+    yrbys = 3.154e7 # yr/s conversion factor
+
+    # Integrate qso emissivity from 1.8 to 10 Ry 
+    n = (qso_emissivity_MH15(z)/(1.70*hplanck) *
+         (1.8**-1.7 - 10.0**-1.7)) # s^-1 Mpc^-3 
+    
+    d = n/nHe - q/trec_HeII(z) # s^-1
+
+    return d * yrbys # yr
+
+
+def dqdt_HeIII(q, z):
+
+    yrbys = 3.154e7 # yr/s conversion factor
+
+    # Integrate qso emissivity from 4 to 10 Ry 
+    n = (qso_emissivity_MH15(z)/(1.70*hplanck) *
+         (4**-1.7 - 10.0**-1.7)) # s^-1 Mpc^-3 
+    
+    d = n/nHe - q/trec_HeIII(z) # s^-1
+
+    return d * yrbys # yr
+
 
 def H(z):
 
@@ -155,6 +250,17 @@ def dqdz_HII(q, z):
 
     return dqdt_HII(q, z)*dtdz(z)
 
+
+def dqdz_HeII(q, z):
+
+    return dqdt_HeII(q, z)*dtdz(z)
+
+
+def dqdz_HeIII(q, z):
+
+    return dqdt_HeIII(q, z)*dtdz(z)
+
+
 def plotq():
 
     fig = plt.figure(figsize=(6, 6), dpi=100)
@@ -168,17 +274,26 @@ def plotq():
 
     z = np.linspace(12, 4, 1000)
     q0 = 1.0e-10
+
     q = odeint(dqdz_HII, q0, z)
     q = np.where(q<1.0, q, 1.0) 
-    plt.plot(z, q, c='k', lw=3, label='MH15 (AGN only)')
+    plt.plot(z, q, c='k', lw=3, label='H~II')
+
+    q = odeint(dqdz_HeII, q0, z)
+    q = np.where(q<1.0, q, 1.0) 
+    plt.plot(z, q, c='c', lw=3, label='He~II')
+
+    q = odeint(dqdz_HeIII, q0, z)
+    q = np.where(q<1.0, q, 1.0) 
+    plt.plot(z, q, c='tomato', lw=3, label='He~III')
 
     plt.ylim(0,1.1)
     plt.xlim(4,15)
 
-    plt.legend(loc='upper right', fontsize=10, handlelength=3, frameon=False, framealpha=0.0,
-               labelspacing=.1, handletextpad=0.4, borderpad=0.2)
+    plt.legend(loc='upper right', fontsize=10, handlelength=3,
+               frameon=False, framealpha=0.0, labelspacing=.1,
+               handletextpad=0.4, borderpad=0.2)
     
-
     plt.savefig('q.pdf', bbox_inches='tight')
     return 
 
