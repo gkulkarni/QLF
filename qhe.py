d0510f27aa723372239eb8c149ca61c3bbf9fc42
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-09-12 15:45:35 +0100

    Calculate and plot Q_He

--- qhe.py
+++ qhe.py
@@ -5,6 +5,9 @@ See also: q.py.
 """
 
 import numpy as np
+from scipy.integrate import odeint
+from scipy.interpolate import RectBivariateSpline
+
 if __name__ == '__main__':
     import matplotlib as mpl
     mpl.use('Agg') 
@@ -13,10 +16,11 @@ if __name__ == '__main__':
     mpl.rcParams['font.serif'] = 'cm'
     mpl.rcParams['font.size'] = '22'
     import matplotlib.pyplot as plt
-    from scipy.integrate import odeint
     import sys 
 
 hplanck = 6.62e-27
+c_angPerSec = 2.998e18
+nu0 = 3.288e15 # threshold freq for H I ionization; s^-1 (Hz)
 
 cosmo_params_MH15 = True 
 if cosmo_params_MH15:
@@ -29,6 +33,24 @@ else:
     omega_b = 0.0482
     omega_lambda = 0.692
     omega_nr = 0.308
+
+z_HM12 = np.loadtxt('z_HM12.txt')
+data_HM12 = np.loadtxt('hm12.txt')
+w_HM12 = data_HM12[:,0]
+e_HM12 = data_HM12[:,1:]
+
+# kx=1 and ky=1 required here to get good interpolation at the Lyman
+# break.  Also remember that you will most likely need the
+# 'grid=False' option while invoking emissivity_HM12.
+emissivity_HM12 = RectBivariateSpline(w_HM12, z_HM12, e_HM12, kx=1, ky=1)
+
+
+def em_hm12(w, z):
+
+    """HM12 Galaxies+QSO emissivity."""
+
+    return emissivity_HM12(w, z*np.ones_like(w), grid=False)
+
 
 Y_He = 0.24
 h = 0.678
@@ -51,6 +73,16 @@ def qso_emissivity_HM12(z):
     return 10.0**24.6 * (1.0+z)**4.68 * np.exp(-0.28*z)/(np.exp(1.77*z)+26.3) 
 
 
+def qso_emissivity_m18(z):
+
+     return 10.0**(26.462071*np.exp(-1.623373e-2*z)-3.47793833*np.exp(-8.282491e-1*z))
+
+
+def qso_emissivity_m20(z):
+
+     return 10.0**(2.724976e1*np.exp(-2.385609e-2*z)-4.397757*np.exp(-7.115348e-1*z))
+
+ 
 def plot_epsqso():
     fig = plt.figure(figsize=(6, 6), dpi=100)
     ax = fig.add_subplot(1, 1, 1)
@@ -191,12 +223,30 @@ def dqdt_HII(q, z, emissivity):
     yrbys = 3.154e7 # yr/s conversion factor
 
     # Integrate qso emissivity from 1 to 4 Ry 
-    n = (qso_emissivity_MH15(z)/(1.70*hplanck) -
-         qso_emissivity_MH15(z)*0.25**1.7/(1.70*hplanck)) # s^-1 Mpc^-3 
+    n = (emissivity(z)/(1.70*hplanck) -
+         emissivity(z)*0.25**1.7/(1.70*hplanck)) # s^-1 Mpc^-3 
     
     d = n/nH - q/trec_HII(z) # s^-1
 
     return d * yrbys # yr
+
+
+def dqdt_HII_HM12(q, z):
+
+    yrbys = 3.154e7 # yr/s conversion factor
+    nu = np.logspace(np.log10(nu0), 20, num=5000)
+    w = c_angPerSec/nu
+    e = em_hm12(w, z) # ergs/s/Mpc^3/Hz
+    n = np.trapz(e/(hplanck*nu), nu) # s^-1 Mpc^-3 
+    
+    d = n/nH - q/trec_HII(z) # s^-1
+
+    return d * yrbys # yr
+
+
+def dqdz_HII_HM12(q, z):
+
+    return dqdt_HII_HM12(q, z)*dtdz(z)
 
 
 def dqdt_HeIII(q, z, emissivity):
@@ -204,7 +254,7 @@ def dqdt_HeIII(q, z, emissivity):
     yrbys = 3.154e7 # yr/s conversion factor
 
     # Integrate qso emissivity from 4 to 10 Ry 
-    n = (qso_emissivity_MH15(z)/(1.70*hplanck) *
+    n = (emissivity(z)/(1.70*hplanck) *
          (4**-1.7 - 10.0**-1.7)) # s^-1 Mpc^-3 
     
     d = n/nHe - q/trec_HeIII(z) # s^-1
@@ -242,26 +292,55 @@ def dqdz_HeIII(q, z, emissivity):
 
 def plotq():
 
-    fig = plt.figure(figsize=(6, 6), dpi=100)
+    fig = plt.figure(figsize=(7, 7), dpi=100)
     ax = fig.add_subplot(1, 1, 1)
     ax.set_ylabel(r'$Q_V$')
     ax.set_xlabel('$z$')
 
+    plt.minorticks_on()
     ax.tick_params('both', which='major', length=7, width=1)
     ax.tick_params('both', which='minor', length=3, width=1)
     ax.tick_params('x', which='major', pad=6)
 
     z = np.linspace(12, 2, 1000)
     q0 = 1.0e-10
-
     q = odeint(dqdz_HII, q0, z, args=(qso_emissivity_MH15,))
     q = np.where(q<1.0, q, 1.0) 
-    plt.plot(z, q, c='k', lw=3, label=r'H~\textsc{ii} (Madau and Haardt 2015; AGN only)')
+    plt.plot(z, q, c='k', lw=2, label=r'H~\textsc{ii} (MH15; AGN only)')
 
+    q_file = 'hm12/q.dat'
+    z_model, q_model = np.loadtxt(q_file, usecols=(0,1), unpack=True)
+    q_model = np.where(q_model<1.0, q_model, 1.0) 
+    plt.plot(z_model, q_model, lw=2, dashes=[7,2], c='k', label=r'H~\textsc{ii} (HM12; Galaxies+AGN)', zorder=-4)
+
+    # z = np.linspace(12, 2, 1000)
+    # q0 = 1.0e-10
+    # q = odeint(dqdz_HII, q0, z, args=(qso_emissivity_m18,))
+    # q = np.where(q<1.0, q, 1.0) 
+    # plt.plot(z, q, c='k', lw=3, label=r'H~\textsc{ii} (our model $M<-18$)', zorder=-3)
+
+    z = np.linspace(12, 2, 1000)
+    q0 = 1.0e-10
     q = odeint(dqdz_HeIII, q0, z, args=(qso_emissivity_MH15,))
     q = np.where(q<1.0, q, 1.0) 
-    plt.plot(z, q, c='tomato', lw=3, label=r'He~\textsc{iii} (Madau and Haardt 2015)')
+    plt.plot(z, q, c='tomato', lw=2, label=r'He~\textsc{iii} (MH15)', zorder=-3)
 
+    z_model, q_model = np.loadtxt(q_file, usecols=(0,2), unpack=True)
+    q_model = np.where(q_model<1.0, q_model, 1.0) 
+    plt.plot(z_model, q_model, lw=2, dashes=[7,2], c='tomato', label=r'He~\textsc{iii} (HM12)', zorder=-5)
+
+    z = np.linspace(12, 2, 1000)
+    q0 = 1.0e-10
+    q = odeint(dqdz_HeIII, q0, z, args=(qso_emissivity_m18,))
+    q = np.where(q<1.0, q, 1.0) 
+    plt.plot(z, q, c='forestgreen', lw=3, label=r'He~\textsc{iii} (our model $M<-18$)', zorder=-3)
+
+    z = np.linspace(12, 2, 1000)
+    q0 = 1.0e-10
+    q = odeint(dqdz_HeIII, q0, z, args=(qso_emissivity_m20,))
+    q = np.where(q<1.0, q, 1.0) 
+    plt.plot(z, q, c='goldenrod', lw=3, label=r'He~\textsc{iii} (our model $M<-20$)', zorder=-3)
+    
     plt.ylim(0,1.1)
     plt.xlim(2,15)
 
