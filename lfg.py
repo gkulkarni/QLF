2aeaa73968154f7bb0e8750d03246776bb92c1be
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-11-14 15:32:23 +0000

    Model 5

--- lfg.py
+++ lfg.py
@@ -20,7 +20,9 @@ qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/willott10_cfhqsdeepsample.dat',
              'Data_new/willott10_cfhqsvwsample.dat',
              'Data_new/kashikawa15_sample.dat',
-             'Data_new/giallongo15_sample.dat']
+             'Data_new/giallongo15_sample.dat',
+             'Data_new/ukidss_sample.dat',
+             'Data_new/banados_sample.dat']
 
 selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
@@ -38,7 +40,9 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10),
              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
-             ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7)]
+             ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7),
+             ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
+             ('Data_new/banados_sel.dat', 1.0, 1.6, 2500.0, 20)]
 
 lfg = lf(quasar_files=qlumfiles, selection_maps=selnfiles, pnum=[3,4,2,5])
 
@@ -61,7 +65,7 @@ lfg.prior_min_values = np.array([-15.0, 0.0, -5.0, -30.0, -10.0, 0.0, -2.0, -7.0
 lfg.prior_max_values = np.array([-5.0, 10.0, 5.0, -10.0, -1.0, 2.0, 2.0, -1.0, 5.0, 0.0, 5.0, 5.0, 10.0, 2.0])
 assert(np.all(lfg.prior_min_values < lfg.prior_max_values))
 
-import bins
+#import bins
 
 #lfg.run_mcmc()
 
