5aaf56234f5aac60ea79ee8738ef33d048939159
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-11-15 17:02:20 +0000

    Model 11

--- lfg.py
+++ lfg.py
@@ -20,9 +20,9 @@ qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/willott10_cfhqsdeepsample.dat',
              'Data_new/willott10_cfhqsvwsample.dat',
              'Data_new/kashikawa15_sample.dat',
-             'Data_new/giallongo15_sample.dat']
-             # 'Data_new/ukidss_sample.dat',
-             # 'Data_new/banados_sample.dat']
+             #'Data_new/giallongo15_sample.dat',
+             'Data_new/ukidss_sample.dat',
+             'Data_new/banados_sample.dat']
 
 selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/croom09sgp_selfunc.dat', 0.3, 0.05, 64.2, 15),
@@ -40,9 +40,9 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/willott10_cfhqsdeepsel.dat', 0.1, 0.025, 4.47, 10),
              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
-             ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7)]
-             # ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
-             # ('Data_new/banados_sel.dat', 1.0, 1.6, 2500.0, 20)]
+             #('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7)
+             ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
+             ('Data_new/banados_sel.dat', 1.0, 1.6, 2500.0, 20)]
 
 case = 0
 
