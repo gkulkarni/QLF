a234cad89b84fda8f4eae1f514dc926cc5a56be0
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-11-16 10:49:20 +0000

    Model 14

--- lfg.py
+++ lfg.py
@@ -21,7 +21,7 @@ qlumfiles = ['Data_new/dr7z2p2_sample.dat',
              'Data_new/willott10_cfhqsvwsample.dat',
              'Data_new/kashikawa15_sample.dat',
            # 'Data_new/giallongo15_sample.dat',
-           # 'Data_new/ukidss_sample.dat']
+             'Data_new/ukidss_sample.dat',
              'Data_new/banados_sample.dat']
 
 selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
@@ -41,8 +41,8 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
            # ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7),
-           # ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
-             ('Data_new/banados_sel.dat', 1.0, 1.6, 2500.0, 20)]
+             ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
+             ('Data_new/banados_sel.dat', 3.0, 1.6, 2500.0, 20)]
 
 case = 0
 
@@ -70,7 +70,7 @@ if case == 0:
     b = lfg.bestfit(g, method=method)
     print b
 
-    import bins
+    #import bins
 
     # lfg.run_mcmc()
 
