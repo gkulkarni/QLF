7572a98f7026e5cfb3bffaadefdfadadd9220f44
Girish Kulkarni <kulkarni@ast.cam.ac.uk>
2017-11-20 14:56:00 +0000

    Model 16

--- lfg.py
+++ lfg.py
@@ -41,8 +41,8 @@ selnfiles = [('Data_new/dr7z2p2_selfunc.dat', 0.1, 0.05, 6248.0, 13),
              ('Data_new/willott10_cfhqsvwsel.dat', 0.1, 0.025, 494.0, 10),
              ('Data_new/kashikawa15_sel.dat', 0.05, 0.05, 6.5, 11),
              ('Data_new/giallongo15_sel.dat', 0.0, 0.0, 0.047, 7),
-             ('Data_new/ukidss_sel.dat', 0.8, 0.45, 3370.0, 19),
-             ('Data_new/banados_sel.dat', 3.0, 1.6, 2500.0, 20)]
+             ('Data_new/ukidss_sel_2.dat', 0.1, 0.1, 3370.0, 19),
+             ('Data_new/banados_sel_2.dat', 0.1, 0.1, 2500.0, 20)]
 
 case = 0
 
