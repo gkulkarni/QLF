d7d6adc1d957c7133422de2d4865e1162b66ea54
Girish Kulkarni <kulkarni@theory.tifr.res.in>
2019-02-28 12:49:34 +0000

    Works correctly now

--- rhoqso_data.py
+++ rhoqso_data.py
@@ -9,8 +9,33 @@ mpl.rcParams['font.size'] = '16'
 import matplotlib.pyplot as plt
 from astropy.stats import knuth_bin_width  as kbw
 from astropy.stats import poisson_conf_interval as pci
+import individual
+
+selnfiles = []
+
+# [('smooth_maps/dr7z2p2_selfunc_with_tiles.dat', 6248.0, 13, r'Richards et al.\ 2006'),
+#              ('smooth_maps/croom09sgp_selfunc_with_tiles.dat', 64.2, 15, r'Croom et al.\ 2009'),
+#              ('smooth_maps/croom09ngp_selfunc_with_tiles.dat', 127.7, 15, r'Croom et al.\ 2009'),
+#              ('smooth_maps/ross13_selfunc2_with_tiles.dat', 2236.0, 1, r'Ross et al.\ 2013'),
+#              ('smooth_maps/dr7z3p7_selfunc_with_tiles.dat', 6248.0, 13, r'Richards et al.\ 2006'),
+#              ('smooth_maps/glikman11_selfunc_ndwfs_with_tiles.dat', 1.71, 6, r'Glikman et al.\ 2011'),
+#              ('smooth_maps/glikman11_selfunc_dls_with_tiles.dat', 2.05, 6, r'Glikman et al.\ 2011'),
+#              ('smooth_maps/yang16_sel_with_tiles.dat', 14555.0, 17, r'Yang et al.\ 2016'),
+#              ('smooth_maps/mcgreer13_dr7selfunc_with_tiles.dat', 6248.0, 8, r'McGreer et al.\ 2013'),
+#              ('smooth_maps/mcgreer13_s82selfunc_with_tiles.dat', 235.0, 8, r'McGreer et al.\ 2013'),
+#              ('smooth_maps/jiang16main_selfunc_with_tiles.dat', 11240.0, 18, r'Jiang et al.\ 2016'),
+#              ('smooth_maps/jiang16overlap_selfunc_with_tiles.dat', 4223.0, 18, r'Jiang et al.\ 2016'),
+#              ('smooth_maps/jiang16s82_selfunc_with_tiles.dat', 277.0, 18, r'Jiang et al.\ 2016'),
+#              ('smooth_maps/willott10_cfhqsdeepsel_with_tiles.dat', 4.47, 10, r'Willott et al.\ 2010'),
+#              ('smooth_maps/willott10_cfhqsvwsel_with_tiles.dat', 494.0, 10, r'Willott et al.\ 2010'),
+#              ('smooth_maps/kashikawa15_sel_with_tiles.dat', 6.5, 11, r'Kashikawa et al.\ 2015')]
+
+# smoothmaps = [individual.selmap(x) for x in selnfiles]
 
 def f(loglf, theta, m, z, fit='individual'):
+    """Return 10**log10phi.
+
+    """
 
     if fit == 'composite':
         return 10.0**loglf(theta, m, z)
@@ -19,6 +44,9 @@ def f(loglf, theta, m, z, fit='individual'):
 
 
 def rhoqso(loglf, theta, mlim, z, fit='individual', mbright=-35.0):
+    """Integrate integral of the LF over magnitudes from mlim to mbright.
+
+    """
 
     m = np.linspace(mbright, mlim, num=1000)
     if fit == 'composite':
@@ -30,6 +58,10 @@ def rhoqso(loglf, theta, mlim, z, fit='individual', mbright=-35.0):
 
 
 def get_rhoqso(lfi, mlim, z, fit='individual', mbright=-35.0):
+    """
+    Compute number density of quasars in object lfi. 
+
+    """
 
     rindices = np.random.randint(len(lfi.samples), size=300)
     n = np.array([rhoqso(lfi.log10phi, theta, mlim, z, mbright=mbright) 
@@ -74,7 +106,42 @@ def binVol(selmap, mrange, zrange):
     return v
 
 
-def totBinVol(lf, m, mbins, selmaps):
+def binVol2(selmap, mrange, zrange):
+
+    """
+
+    Calculate volume in an M-z bin for *one* selmap.
+
+    """
+
+    M_bin_l = mrange[0]
+    M_bin_u = mrange[1]
+
+    assert(M_bin_l < M_bin_u) 
+
+    v = 0.0
+    for i in xrange(selmap.m.size):
+        if (selmap.z[i] >= zrange[0]) and (selmap.z[i] < zrange[1]):
+            M_tile_l = selmap.m[i] - selmap.dm[i]/2.0
+            M_tile_u = selmap.m[i] + selmap.dm[i]/2.0
+
+            assert(M_tile_l < M_tile_u) 
+
+            if (M_bin_l <= M_tile_l < M_bin_u) and (M_bin_l <= M_tile_u < M_bin_u):
+                v += selmap.volarr[i]*selmap.p[i]*selmap.dm[i]
+            elif (M_bin_l <= M_tile_l < M_bin_u):
+                dM = M_bin_u - M_tile_l
+                v += selmap.volarr[i]*selmap.p[i]*dM
+            elif (M_bin_l <= M_tile_u < M_bin_u):
+                dM = M_tile_u - M_bin_l
+                v += selmap.volarr[i]*selmap.p[i]*dM 
+            else:
+                v += selmap.volarr[i]*selmap.p[i]*(M_bin_u-M_bin_l) 
+            
+    return v
+
+
+def totBinVol(lf, m, mbins, selmaps, use_binvol2=False):
 
     """
 
@@ -88,31 +155,44 @@ def totBinVol(lf, m, mbins, selmaps):
     mhigh = mbins[idx]
     mrange = (mlow, mhigh)
 
-    v = np.array([binVol(x, mrange, lf.zlims) for x in selmaps])
+    if use_binvol2: 
+        v = np.array([binVol2(x, mrange, lf.zlims) for x in selmaps])
+    else:
+        v = np.array([binVol(x, mrange, lf.zlims) for x in selmaps])
+
     total_vol = v.sum() 
 
     return total_vol
 
 
-def get_rhoqso(lf, sid, Mlim):
-
-    m = lf.M1450[lf.sid==sid]
-    selmaps = [x for x in lf.maps if x.sid == sid]
-    n = np.size(m[m < Mlim])
-    v = np.array([cumVol(x, Mlim, lf.zlims) for x in selmaps])
-
-    return n/np.sum(v)
-
-
-def get_lf(lf, sid, bins):
+def get_lf(lf, sid, bins, use_smooth_maps=False, use_binvol2 = False):
     
     # Bin data.  This is only for visualisation and to compare
     # with reported binned values.  
     m = lf.M1450[lf.sid==sid]
 
-    selmaps = [x for x in lf.maps if x.sid == sid]
+    #print 'Originally have {:d} quasars'.format(np.size(m))
 
-    v1 = np.array([totBinVol(lf, x, bins, selmaps) for x in m])
+
+    #print 'Should have {:d} quasars'.format(np.size(m[m<-24.0]))
+    
+    ms = (bins[1:]+bins[:-1])*0.5
+
+    limit = np.max(ms)
+    m = m[m<-24.0]
+    # print 'bins=', bins
+    #print 'limit=', limit
+
+    #print 'Have {:d} quasars'.format(np.size(m))
+
+    
+
+    if use_smooth_maps:
+        selmaps = [x for x in smoothmaps if x.sid == sid]
+    else:
+        selmaps = [x for x in lf.maps if x.sid == sid]
+
+    v1 = np.array([totBinVol(lf, x, bins, selmaps, use_binvol2=use_binvol2) for x in m])
 
     v1_nonzero = v1[np.where(v1>0.0)]
     m = m[np.where(v1>0.0)]
@@ -141,8 +221,20 @@ def get_lf(lf, sid, bins):
     uperr = np.log10(nlims[1]) - logphi 
     downerr = logphi - np.log10(nlims[0])
 
+    # for a in zip(mags, n):
+    #     print '{:.2f}  {:d}'.format(a[0], a[1])
+
     return mags, left, right, logphi, uperr, downerr
 
+
+def get_rhoqso(lf, sid, Mlim):
+
+    m = lf.M1450[lf.sid==sid]
+    selmaps = [x for x in lf.maps if x.sid == sid]
+    n = np.size(m[m < Mlim])
+    v = np.array([cumVol(x, Mlim, lf.zlims) for x in selmaps])
+
+    return n/np.sum(v)
 
 def rhoqso2(lfs, mag_threshold):
 
@@ -204,7 +296,7 @@ def rhoqso_old(lfs, mag_threshold, bins):
     return zs, rhos 
 
 
-def rhoqso3(lfs, mag_threshold, bins):
+def rhoqso3(lfs, mag_threshold, bins, **kwargs):
 
     zs = []
     rhos = [] 
@@ -218,16 +310,73 @@ def rhoqso3(lfs, mag_threshold, bins):
 
         logphis = []
         for sid in sids: 
-            mags, left, right, logphi, uperr, downerr = get_lf(x, sid, bins)
+            mags, left, right, logphi, uperr, downerr = get_lf(x, sid, bins, **kwargs)
             logphis.append(10.0**logphi)
-        logphi = np.sum(logphis, axis=0)
+
+        Nbins = np.size(mags)
+        Nsamples = len(logphis)
+        print 'Nbins=', Nbins
+        print 'Nsamples=', Nsamples
+        total_phi = np.zeros(Nbins)
+        
+        for i in range(Nbins):
+            vals = []
+            nonzero_counter = 0 
+            for j in range(Nsamples):
+                if logphis[j][i] > 0.0:
+                    nonzero_counter += 1 
+                    vals.append(logphis[j][i])
+            if nonzero_counter > 0:
+                total_phi[i] = np.mean(np.array(vals))
+            # print i, total_phi[i]
+                        
+            
+
+        #     N = len(logphis)
+        # if N > 2:
+        #     print 'We have a problem: N > 2!!!'
+            
+        # if N > 1:
+        #     total_phi = logphis[0]
+        #     for ns in range(1,N):
+        #         for i, p in enumerate(logphis[ns]):
+        #             if total_phi[i] == 0.0:
+        #                 total_phi[i] = total_phi[i] + p
+        #             elif p > 0.0:
+        #                 total_phi[i] = (total_phi[i] + p)/2 
+        #     logphi = total_phi
+        # else:
+        #     logphi = np.sum(logphis, axis=0)
+
+        logphi = total_phi 
+        
+        #row1 = logphis[0]
+        #row2 = logphis[1]
+
+        # logphi = np.sum(logphis, axis=0)
+
+        # #logphi = total_phi 
+
+        # logphi_test = np.mean(logphis, axis=0)
+
+        #for phivalue in zip(row1, row2, logphi):
+         #   print '{:.2e}  {:.2e}  {:.2e}'.format(*phivalue)
+        
+            #print logphi 
 
         dm = left + right
         phidm = logphi * dm 
         intphidm = np.cumsum(phidm)
 
+        # for omega in zip(mags, logphi):
+        #     print '{:.2f}  {:.2e}'.format(omega[0], omega[1])
+
+        # print 'mags=', mags
+        # print 'logphi=', logphi 
+        #
         assert(np.all(np.diff(mags) > 0))
-        rho = np.interp(mag_threshold, mags, intphidm)
+        # rho = np.interp(mag_threshold, mags, intphidm)
+        rho = intphidm[-1]
         nqso = np.size(x.M1450[x.M1450 < mag_threshold])
 
         if nqso > 0: 
@@ -299,29 +448,29 @@ def draw_withGlobal_multiple(c1, c2, c3, individuals, select=False):
     ax.set_xlim(0.,7)
 
     ax.set_yscale('log')
-    ax.set_ylim(1.0e-10, 1.0e-3)
+    ax.set_ylim(1.0e-10, 1.0e-5)
 
-    mlim = -21
-    zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.6))
-    ax.scatter(zs, rhos, c='forestgreen', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-21$') 
-    ax.errorbar(zs, rhos, ecolor='forestgreen', capsize=0, fmt='None', elinewidth=1, 
-                yerr=np.vstack((downerr, uperr)),
-                #xerr=np.vstack((lzerr, uzerr)), 
-                zorder=10, mew=1, ms=5) 
-    m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
-    m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
-    m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')
+    # mlim = -21
+    # zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.6))
+    # ax.scatter(zs, rhos, c='forestgreen', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-21$') 
+    # ax.errorbar(zs, rhos, ecolor='forestgreen', capsize=0, fmt='None', elinewidth=1, 
+    #             yerr=np.vstack((downerr, uperr)),
+    #             #xerr=np.vstack((lzerr, uzerr)), 
+    #             zorder=10, mew=1, ms=5) 
+    # m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
+    # m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
+    # m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')
     
-    mlim = -23
-    zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
-    ax.scatter(zs, rhos, c='red', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-23$') 
-    ax.errorbar(zs, rhos, ecolor='red', capsize=0, fmt='None', elinewidth=1, 
-                yerr=np.vstack((downerr, uperr)),
-                #xerr=np.vstack((lzerr, uzerr)), 
-                zorder=10, mew=1, ms=5)
-    m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
-    m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
-    m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
+    # mlim = -23
+    # zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
+    # ax.scatter(zs, rhos, c='red', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-23$') 
+    # ax.errorbar(zs, rhos, ecolor='red', capsize=0, fmt='None', elinewidth=1, 
+    #             yerr=np.vstack((downerr, uperr)),
+    #             #xerr=np.vstack((lzerr, uzerr)), 
+    #             zorder=10, mew=1, ms=5)
+    # m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
+    # m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
+    # m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
 
     mlim = -24
     zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
@@ -334,10 +483,10 @@ def draw_withGlobal_multiple(c1, c2, c3, individuals, select=False):
     m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
     m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
 
-    mlim = -25
+    mlim = -25.5
     zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
-    ax.scatter(zs, rhos, c='peru', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-25$') 
-    ax.errorbar(zs, rhos, ecolor='peru', capsize=0, fmt='None', elinewidth=1,
+    ax.scatter(zs, rhos, c='forestgreen', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-25$') 
+    ax.errorbar(zs, rhos, ecolor='forestgreen', capsize=0, fmt='None', elinewidth=1,
                 yerr=np.vstack((downerr, uperr)),
                 #xerr=np.vstack((lzerr, uzerr)), 
                 zorder=10, mew=1, ms=5)
@@ -345,21 +494,21 @@ def draw_withGlobal_multiple(c1, c2, c3, individuals, select=False):
     m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
     m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
     
-    mlim = -26
-    zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
-    ax.scatter(zs, rhos, c='tomato', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-26$') 
-    ax.errorbar(zs, rhos, ecolor='tomato', capsize=0, fmt='None', elinewidth=1,
-                yerr=np.vstack((downerr, uperr)),
-                #xerr=np.vstack((lzerr, uzerr)), 
-                zorder=10, mew=1, ms=5)
-    m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
-    m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
-    m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
+    # mlim = -26
+    # zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
+    # ax.scatter(zs, rhos, c='tomato', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-26$') 
+    # ax.errorbar(zs, rhos, ecolor='tomato', capsize=0, fmt='None', elinewidth=1,
+    #             yerr=np.vstack((downerr, uperr)),
+    #             #xerr=np.vstack((lzerr, uzerr)), 
+    #             zorder=10, mew=1, ms=5)
+    # m1f, m1 = global_cumulative(ax, c1, mlim, 'grey')
+    # m2f, m2 = global_cumulative(ax, c2, mlim, 'forestgreen')
+    # m3f, m3 = global_cumulative(ax, c3, mlim, 'peru')    
     
     mlim = -27
     zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, -17.3, 0.1))
-    ax.scatter(zs, rhos, c='saddlebrown', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-27$') 
-    ax.errorbar(zs, rhos, ecolor='saddlebrown', capsize=0, fmt='None', elinewidth=1,
+    ax.scatter(zs, rhos, c='tomato', edgecolor='None', s=42, zorder=10, linewidths=2, label='$M_{1450}<-27$') 
+    ax.errorbar(zs, rhos, ecolor='tomato', capsize=0, fmt='None', elinewidth=1,
                 yerr=np.vstack((downerr, uperr)),
                 #xerr=np.vstack((lzerr, uzerr)), 
                 zorder=10, mew=1, ms=5)
@@ -373,6 +522,41 @@ def draw_withGlobal_multiple(c1, c2, c3, individuals, select=False):
                scatterpoints=1)
     
     plt.savefig('rhoqso_data.pdf',bbox_inches='tight')
+    plt.close('all')
+
+    return
+
+
+def test(c1, individuals, **kwargs):
+
+    fig = plt.figure(figsize=(7, 11), dpi=100)
+    ax = fig.add_subplot(1, 1, 1)
+
+    ax.tick_params('both', which='major', length=7, width=1)
+    ax.tick_params('both', which='minor', length=5, width=1)
+
+    ax.set_ylabel(r'$\rho(z, M_{1450} < M_\mathrm{lim})$ [cMpc$^{-3}$]')
+    ax.set_xlabel('$z$')
+    ax.set_xlim(0.,7)
+
+    ax.set_yscale('log')
+    ax.set_ylim(1.0e-10, 1.0e-3)
+
+    mlim = -24
+
+    dm = 0.6
+    bins = np.arange(mlim+dm/2, -35, -dm)[::-1]
+    zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=bins)
+    print 'rhos=', rhos
+
+    # zs, rhos, uperr, downerr = rhoqso3(individuals, mlim, bins=np.arange(-30.9, mlim+0.6, 0.6))
+    # print 'rhos=', rhos
+
+    ax.scatter(zs, rhos, c='b', edgecolor='None', s=42, zorder=10, linewidths=2, label='$0.6$')
+
+    global_cumulative(ax, c1, mlim, 'grey')
+
+    plt.savefig('rhoqso_test.pdf',bbox_inches='tight')
     plt.close('all')
 
     return
